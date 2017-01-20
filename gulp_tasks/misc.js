9894b262db512075de76797a48c08177d6af1558
Abdullah Alansari <ahimta@gmail.com>
2017-01-20 16:38:09 +0300

    Add gulp tasks

--- gulp_tasks/misc.js
+++ gulp_tasks/misc.js
@@ -2,16 +2,30 @@ const path = require('path');
 
 const gulp = require('gulp');
 const del = require('del');
+const appcache = require('gulp-appcache');
 const filter = require('gulp-filter');
 const rename = require('gulp-rename');
 
 const conf = require('../conf/gulp.conf');
 
+gulp.task('appcache', generateManifest);
 gulp.task('clean', clean);
 gulp.task('other', other);
 
 function clean() {
   return del([conf.paths.dist, conf.paths.tmp]);
+}
+
+function generateManifest() {
+  return gulp.src(conf.path.dist('**/*'))
+    .pipe(appcache({
+      hash: true,
+      network: ['*'],
+      preferOnline: false,
+      filename: 'app.manifest',
+      exclude: 'app.manifest'
+    }))
+    .pipe(gulp.dest(conf.path.dist()));
 }
 
 function other() {
